3a2255019b7e043f91f0f88ead7ee2de26e16bd1
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-06-12 14:53:42 -0300

    add CrosOrigin

--- CardapioJavaSpring/src/main/java/br/edu/cardapiojavaspring/controller/FoodController.java
+++ CardapioJavaSpring/src/main/java/br/edu/cardapiojavaspring/controller/FoodController.java
@@ -3,6 +3,7 @@ package br.edu.cardapiojavaspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +22,14 @@ public class FoodController {
 	@Autowired
 	private FoodRepository repository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public List<FoodResponseDTO> getAll() {
 		List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
 		return foodList;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public void saveFood(@RequestBody FoodRequestDTO data) {
 		Food foodData = new Food(data);
